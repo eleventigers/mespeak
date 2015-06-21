f0c80b2b99ce443168b7ef5b606dc036c58a7262
eleventigers <phonodelik@gmail.com>
2015-06-21 12:24:58 +0100

    Fix node example

--- example/node.js
+++ example/node.js
@@ -5,7 +5,7 @@ mespeak.loadConfig(require("../src/mespeak_config.json"));
 mespeak.loadVoice(require("../voices/en/en-us.json"))
 
 var data = mespeak.speak("hello", {
-	rawdata: true
+	rawdata: "buffer"
 });
 
 fs.writeFileSync("test.wav",data);
